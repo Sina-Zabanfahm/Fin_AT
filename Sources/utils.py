869c17855662de175945a667aea977ae8c951573
Sina Zabanfahm <sinazabanfahm@Sinas-MacBook-Air.local>
2024-10-26 15:25:38 -0400

    docs for yf.Ticker(AAPL)

--- Sources/utils.py
+++ Sources/utils.py
@@ -1,6 +1,6 @@
 from typing import Any
-def generate_docs(obj: Any, isSaved:bool = True, 
-                  folder_path: str = '/Users/sinazabanfahm/AlgoT/Sources') -> str:
+def generate_docs(obj: Any, isSaved:bool = True,
+                  folder_path: str = '/Users/sinazabanfahm/FinAT/Sources/docs') -> str:
     opt = ""
     folder_path = folder_path + "/" if folder_path[-1]!="/" else folder_path
     file_path = folder_path + str(obj) +".txt"
@@ -18,6 +18,6 @@ def generate_docs(obj: Any, isSaved:bool = True,
         with open(file_path, 'w') as file:
             file.write(opt)
     return opt
-#import yfinance as yf
-#obj = yf.Ticker("AAPL")
-#generate_docs(obj)
+# import yfinance as yf
+# obj = yf.Ticker("AAPL")
+# generate_docs(obj)
